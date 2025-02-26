3dc6acb815ebf249471745aa4a6acf8773fee44e
GUILHERME LUAN SILVA DOS REIS <guilherme.luan@a.ucb.br>
2025-02-26 10:23:31 -0300

    first commit

---
+++ Aula 1/strings_built-in.py
@@ -0,0 +1,14 @@
+# Strings Built-in Functions
+
+course_name = "Python Programming"
+
+print(len(course_name)) # len(): Retorna tamanho da String
+print(course_name[0])   # []: Acessa o caractere na posição 0
+print(course_name[-1])  # []: Acessa o último caractere -1
+print(course_name[0:3]) # [0:3]: Acessa os caracteres de 0 a 2
+print(course_name[1:3])  # [1:3]: Acessa os caracteres de 1 a 2
+print(course_name[3:])  # [3:]: Acessa os caracteres a partir da posição 3
+
+
+
+
